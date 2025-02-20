f5461500719e24535787a9f0b2a934a52d2dbec4
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-02-20 19:43:38 +0100

    Rename settings table to config in migration file

--- database/migrations/2025_02_20_183647_crete_config_table.php
+++ database/migrations/2025_02_20_183647_crete_config_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('settings', function (Blueprint $table) {
+        Schema::create('config', function (Blueprint $table) {
             $table->id();
             $table->text('nav_items');
             $table->text('movies');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('settings');
+        Schema::dropIfExists('config');
     }
 };

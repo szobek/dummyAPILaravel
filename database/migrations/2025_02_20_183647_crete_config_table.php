3b4e9f049b01a6359a1937916730708169584205
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-02-20 19:41:57 +0100

    Remove unused migration files and update settings migration to create settings table

--- database/migrations/2025_02_20_183647_crete_config_table.php
+++ database/migrations/2025_02_20_183647_crete_config_table.php
@@ -11,7 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->text('nav_items');
+            $table->text('movies');
+            $table->text('subscriptions');
+            $table->text('voluntaries');
+            $table->smallInteger('user_id');
+            $table->smallInteger('invoices_limit');
+            $table->string('dummy_url');
+            $table->string('dummy_pdf_url');
+            $table->string('dummy_zip_url');
+            $table->string('base_url');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('settings');
     }
 };

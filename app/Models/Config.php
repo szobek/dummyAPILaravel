c4932e23ace2a3d459dbb7be09e01de234d94da2
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-02-20 20:21:39 +0100

    Add Config model with fillable attributes for configuration data

---
+++ app/Models/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Config extends Model
+{
+    protected $fillable = [
+        'nav_items', 'user_id', 'invoices_limit', 'dummy_url', 'dummy_pdf_url', 'created_at', 'updated_at',
+    ];
+}
